12695255adc50a25fd3e2d14d35e6e2f3470718b
erharsh237 <erharsh237@gmail.com>
2024-12-31 00:44:04 +0530

    Hello

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter } from 'react-router-dom';
+
+<BrowserRouter basename="/my-website">
+  {
+    <App />
+  }
+</BrowserRouter>
 
 function App() {
   const [count, setCount] = useState(0)
